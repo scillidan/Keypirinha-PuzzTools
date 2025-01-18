7f21ae093fbb7c2d72d30326d40650a11f548df6
scillidan <scillidan@yandex.com>
2025-01-18 14:52:56 +0800

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -15,10 +15,15 @@ def make_grid_transform(grid_transform: GridTransform) -> Callable[[str], str]:
         return new_line.join(('\t' if has_tab else '').join(line) for line in new_grid)
     return transform
 
-_SORTS = {
-    'alphabetical': lambda x: '\n'.join(sorted(x.split('\n'))),
-    'by length': lambda x: '\n'.join(sorted(x.split('\n'), key=len)),
-    'reverse': lambda x: '\n'.join(reversed(x.split('\n'))),
+_CASES = {
+    'lowercase': str.lower,
+    'uppercase': str.upper,
+    'titlecase': lambda x: x.title(),
+    'kebabcase': lambda x: re.sub(r'\s|(\B)([A-Z])', r'-\2', x).lower(),
+    'snakecase': lambda x: re.sub(r'\s|(\B)([A-Z])', r'_\2', x).lower(),
+    'camelcase': lambda x: x[0].lower() + re.sub(r'\s|(\B)([A-Z])', r'\2', x)[1:],
+    'pascalcase': lambda x: re.sub(r'\s|(\B)([A-Z])', r'\2', x),
+    'nocase': str,
 }
 
 _NUTRIMATICS = {
@@ -28,18 +33,33 @@ _NUTRIMATICS = {
     'from enumeration': lambda x: ' '.join(f'A{{{n}}}' for n in re.findall(r'\d+', x)),
 }
 
+_SORTS = {
+    'alphabetical': lambda x: '\n'.join(sorted(x.split('\n'))),
+    'by length': lambda x: '\n'.join(sorted(x.split('\n'), key=len)),
+    'reverse': lambda x: '\n'.join(reversed(x.split('\n'))),
+}
+
+_TOOLS = {
+    '2winurl': lambda x: x.replace('/', '\\'),
+    # '2winurl2': lambda x: x.replace('/', '\\\\'),
+    '2unixurl': lambda x: x.replace('\\', '/'),
+    '2raw': lambda x: re.sub(r'https://github.com/([^/]+)/([^/]+)/blob/([^/]+)/(.+)', r'https://raw.githubusercontent.com/\1/\2/refs/heads\3/\4', x),
+    '2gh': lambda x: re.sub(r'https://github.com/([^/]+)/([^/]+)(?:/.*)?', r'\1/\2', x),
+    '2atomc': lambda x: re.sub(r'https://github.com/([^/]+)/([^/]+)(?:/.*)?', r'https://github.com/\1/\2/commits.atom', x),
+    '2atomr': lambda x: re.sub(r'https://github.com/([^/]+)/([^/]+)(?:/.*)?', r'https://github.com/\1/\2/releases.atom', x),
+    'list2line': lambda x: ','.join(sorted(set(x.split()), key=int))
+}
+
 TRANSFORMS = {
+    'case': _CASES,
+    'nutrimatic': _NUTRIMATICS,
+    'sort': _SORTS,
+    'tool': _TOOLS,
     'alphabet': lambda _: 'abcdefghijklmnopqrstuvwxyz',
     'answerize': lambda x: re.sub('[^A-Z0-9]', '', x.upper()),
-    'kebabcase': lambda x: re.sub(r'\s|(\B)([A-Z])', r'-\2', x).lower(),
     'length': len,
-    'lowercase': str.lower,
-    'nutrimatic': _NUTRIMATICS,
     'reverse': lambda x: x[::-1],
     'rotate': make_grid_transform(lambda x: zip(*reversed(x))),
-    'snakecase': lambda x: re.sub(r'\s|(\B)([A-Z])', r'_\2', x).lower(),
-    'sort': _SORTS,
     'transpose': make_grid_transform(lambda x: zip(*x)),
     'unique': lambda x: ''.join(set(x)),
-    'uppercase': str.upper,
 }
